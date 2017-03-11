435b68bf44122542ae47e077bca07666c1e7c328
Nicole <nmarielockwood@gmail.com>
2017-03-11 12:38:07 -0500

    adding files

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+//SETUP VARIABLES
+/**Initialize Firebase**/
+  var config = {
+    apiKey: "AIzaSyDvXc_KdksH_V5vCF3OMIlH1kLfhG46GQE",
+    authDomain: "rps-multiplayer-e9f2b.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-e9f2b.firebaseio.com",
+    storageBucket: "rps-multiplayer-e9f2b.appspot.com",
+    messagingSenderId: "221954471082"
+  };
+
+    firebase.initializeApp(config);
+
+/**Create a variable to reference the database**/
+var database = firebase.database();
+
+//FUNCTIONS
+
+
+//MAIN PROCESSES
+$(document).ready(function() {
+ 
+ };
